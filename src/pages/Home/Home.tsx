2fda431a46735efef3fea04a60a000df1852797e
Wojciech Malarski <w.malarski@gmail.com>
2022-04-05 18:42:55 +0200

    antd components working

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,5 +1,6 @@
 import { LocationGenerics } from "@/navigation/location";
 import { paths } from "@/navigation/paths";
+import { Button, Layout, Menu } from "antd";
 import { ReactElement } from "react";
 import { useTranslation } from "react-i18next";
 import { Link, Outlet, useLocation } from "react-location";
@@ -9,13 +10,36 @@ export const Home = (): ReactElement => {
 
   const location = useLocation();
 
-  console.log({ location });
-
   return (
-    <div>
-      <p>{t("Home")}</p>
-      <Link<LocationGenerics> to={paths.home}>Home</Link>
-      <Outlet />
-    </div>
+    <Layout
+      style={{ height: "100vh", overflow: "clip", backgroundColor: "white" }}
+    >
+      <Layout.Header>
+        <Menu theme="light" mode="horizontal" defaultSelectedKeys={["2"]}>
+          <Menu.Item key="1">nav 1</Menu.Item>
+          <Menu.Item key="2">nav 2</Menu.Item>
+          <Menu.Item key="3">nav 3</Menu.Item>
+        </Menu>
+      </Layout.Header>
+      <Layout>
+        <Layout.Sider>
+          <Menu theme="light" mode="vertical" defaultSelectedKeys={["2"]}>
+            <Menu.Item key="1">nav 1</Menu.Item>
+            <Menu.Item key="2">nav 2</Menu.Item>
+            <Menu.Item key="3">nav 3</Menu.Item>
+          </Menu>
+        </Layout.Sider>
+        <Layout.Content>
+          main content
+          <Button>Hello</Button>
+          <div>
+            <p>{t("Home")}</p>
+            <Link<LocationGenerics> to={paths.home}>Home</Link>
+            <Outlet />
+          </div>
+        </Layout.Content>
+      </Layout>
+      <Layout.Footer>footer</Layout.Footer>
+    </Layout>
   );
 };
--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,34 @@
 import react from "@vitejs/plugin-react";
 import * as path from "path";
 import { defineConfig } from "vite";
+import { AntdResolve, createStyleImportPlugin } from "vite-plugin-style-import";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    createStyleImportPlugin({
+      resolves: [AntdResolve()],
+      // libs: [
+      //   // If you don’t have the resolve you need, you can write it directly in the lib, or you can provide us with PR
+      //   {
+      //     libraryName: "ant-design-vue",
+      //     esModule: true,
+      //     resolveStyle: (name) => {
+      //       return `ant-design-vue/es/${name}/style/index`;
+      //     },
+      //   },
+      // ],
+    }),
+  ],
   resolve: {
     alias: [{ find: "@", replacement: path.resolve(__dirname, "src") }],
+  },
+  css: {
+    preprocessorOptions: {
+      less: {
+        javascriptEnabled: true,
+      },
+    },
   },
 });
