820b6aa99aa0aa540489520b2f849988176a5b7b
Wojciech Malarski <w.malarski@gmail.com>
2022-04-10 21:39:49 +0200

    spread sheets api

--- src/tests/TestWrapper.tsx
+++ src/tests/TestWrapper.tsx
@@ -1,25 +1,39 @@
 import { Router } from "@/navigation/Router";
-import { SheetApiContext } from "@/services/SheetApi";
-import { SpreadSheetApiContext } from "@/services/SpreadSheetApi";
+import { SheetApiContext, SheetApiService } from "@/services/SheetApi";
+import {
+  SpreadSheetApiContext,
+  SpreadSheetApiService,
+} from "@/services/SpreadSheetApi";
 import i18next from "@/utils/i18next";
 import { ReactElement, ReactNode } from "react";
 import { I18nextProvider } from "react-i18next";
-import { mockSheetApi } from "./mocks";
+import { mockSheetApi, mockSpreadSheetApi } from "./mocks";
 
 export type TestWrapperProps = {
   children?: ReactNode;
+  sheetApi?: SheetApiService;
+  spreadSheetApi?: SpreadSheetApiService;
 };
 
 export type PropsWithTestWrapper<T = unknown> = T & {
   wrapperProps?: Omit<TestWrapperProps, "children">;
 };
 
-export const TestWrapper = ({ children }: TestWrapperProps): ReactElement => {
+export const TestWrapper = ({
+  children,
+  sheetApi,
+  spreadSheetApi,
+}: TestWrapperProps): ReactElement => {
   return (
     <SheetApiContext.Provider
-      value={{ isInitialized: true, api: mockSheetApi() }}
+      value={{ isInitialized: true, api: sheetApi ?? mockSheetApi() }}
     >
-      <SpreadSheetApiContext.Provider>
+      <SpreadSheetApiContext.Provider
+        value={{
+          isInitialized: true,
+          api: spreadSheetApi ?? mockSpreadSheetApi(),
+        }}
+      >
         <Router>
           <I18nextProvider i18n={i18next}>{children}</I18nextProvider>
         </Router>
--- src/tests/mocks.ts
+++ src/tests/mocks.ts
@@ -1,4 +1,5 @@
 import { Sheet, SheetApiService } from "@/services/SheetApi";
+import { SpreadSheet, SpreadSheetApiService } from "@/services/SpreadSheetApi";
 
 export const mockSheet = (update: Partial<Sheet> = {}): Sheet => {
   const id = update.id ?? Math.floor(Math.random() * 1e10);
@@ -55,6 +56,20 @@ export const mockSheetApi = ({
       const next = { ...previous, ...args };
       collection.splice(index, 1, next);
       return Promise.resolve(next);
+    },
+  };
+};
+
+export const mockSpreadSheetApi = (): SpreadSheetApiService => {
+  const collection: SpreadSheet[] = [];
+  return {
+    get: ({ queryKey }) => {
+      const sheet = collection.find((entry) => entry.id == queryKey[1]);
+      if (!sheet) return Promise.reject();
+      return Promise.resolve(sheet);
+    },
+    key: (id) => {
+      return ["spreadSheet", id];
     },
   };
 };
