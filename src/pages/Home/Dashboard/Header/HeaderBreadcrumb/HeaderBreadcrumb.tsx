1ce5b3c144e537e986e1e19934d33081ccceb885
Wojciech Malarski <w.malarski@gmail.com>
2022-04-23 10:47:26 +0200

    breadcrumb fixed

--- src/pages/Home/Dashboard/Header/HeaderBreadcrumb/HeaderBreadcrumb.tsx
+++ src/pages/Home/Dashboard/Header/HeaderBreadcrumb/HeaderBreadcrumb.tsx
@@ -1,18 +1,24 @@
 import { Breadcrumb } from "antd";
 import { ReactElement } from "react";
+import { useTranslation } from "react-i18next";
 import { Link } from "react-router-dom";
-import useBreadcrumbs from "use-react-router-breadcrumbs";
+import useBreadcrumbs, { BreadcrumbsRoute } from "use-react-router-breadcrumbs";
 
 export const HeaderBreadcrumb = (): ReactElement => {
-  const breadcrumbs = useBreadcrumbs();
+  const { t } = useTranslation("common", { keyPrefix: "navigation" });
 
-  const filtered = breadcrumbs.filter((match) => match);
+  const routes: BreadcrumbsRoute<string>[] = [
+    { path: "/", breadcrumb: t("home") },
+    { path: "/sheet/:sheetId", breadcrumb: t("sheet") },
+    { path: "/sheet/:sheetId/settings", breadcrumb: t("settings") },
+    { path: "/sheet/:sheetId/invoice/:invoiceId", breadcrumb: t("invoice") },
+  ];
 
-  console.log({ breadcrumbs });
+  const breadcrumbs = useBreadcrumbs(routes, { disableDefaults: true });
 
   return (
     <Breadcrumb>
-      {filtered.map(({ match, breadcrumb }) => (
+      {breadcrumbs.map(({ match, breadcrumb }) => (
         <Breadcrumb.Item key={match.pathname}>
           <Link to={match.pathname}>{breadcrumb}</Link>
         </Breadcrumb.Item>
