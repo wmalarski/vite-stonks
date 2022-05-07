8fab2e2f9cb43283d06956064485bdb65f6ac910
Wojciech Malarski <w.malarski@gmail.com>
2022-05-07 13:49:06 +0200

    msw models added

--- src/tests/handlers/sheets.ts
+++ src/tests/handlers/sheets.ts
@@ -1,9 +1,52 @@
+import { QuerySelectorWhere } from "@mswjs/data/lib/query/queryTypes";
 import { rest } from "msw";
+import { db } from "../models";
 
 const base = `${import.meta.env.VITE_SUPABASE_URL}/rest/v1/sheets`;
+
+const parseQueryParams = (
+  searchParams: URLSearchParams,
+  excludeKeys: string[] = []
+) => {
+  const paginationKeys = ["offset", "limit", "select"];
+  const rawOffset = searchParams.get("offset");
+  const rawLimit = searchParams.get("limit");
+
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  const where: QuerySelectorWhere<any> = {};
+  const skip = rawOffset == null ? undefined : parseInt(rawOffset, 10);
+  const take = rawLimit == null ? undefined : parseInt(rawLimit, 10);
+
+  searchParams.forEach((value, key) => {
+    if (paginationKeys.includes(key) || excludeKeys.includes(key)) return;
+    where[key] = { equals: value };
+  });
+
+  return { skip, take, where };
+};
+
 export const handlers = [
-  rest.get(`${base}/authorize`, (_req, res, ctx) => {
-    sessionStorage.setItem("is-authenticated", "true");
+  rest.get(base, (req, res, ctx) => {
+    const { skip, take, where } = parseQueryParams(req.url.searchParams);
+
+    const result = db.sheet.findMany({ where, skip, take });
+
+    const actions = actionsEntities.flatMap((entity) => {
+      const action = convert.toAction(entity);
+      return action ? [action] : [];
+    });
+
+    return res(ctx.json<Action[]>(actions));
+
+    /*
+select
+	*
+offset
+	0
+limit
+	11
+*/
+
     return res(ctx.status(200));
   }),
   // rest.post(`${base}/logout`, (_req, res, ctx) => {
--- src/tests/models.ts
+++ src/tests/models.ts
@@ -1,9 +1,22 @@
-import { factory, primaryKey } from "@mswjs/data";
-import { nanoid } from "nanoid";
+import { Company } from "@/services/CompanyApi";
+import { Invoice } from "@/services/InvoiceApi";
+import { Report } from "@/services/ReportApi";
+import { Sheet } from "@/services/SheetApi";
+import { factory, oneOf, primaryKey } from "@mswjs/data";
+import { Entity, Value } from "@mswjs/data/lib/glossary";
 
-export const db = factory({
+export const dbIndexCounter = (() => {
+  let index = 1;
+  return () => {
+    const result = index;
+    index += 1;
+    return result;
+  };
+})();
+
+const directory = {
   sheet: {
-    id: primaryKey(() => nanoid()),
+    id: primaryKey(dbIndexCounter),
     account: String,
     address1: String,
     address2: String,
@@ -15,4 +28,79 @@ export const db = factory({
     nip: String,
     user_id: String,
   },
-});
+  company: {
+    id: primaryKey(dbIndexCounter),
+    address1: String,
+    address2: String,
+    company: String,
+    created_at: String,
+    nip: String,
+    sheet_id: oneOf("sheet"),
+  },
+  invoice: {
+    id: primaryKey(dbIndexCounter),
+    company_id: oneOf("company"),
+    created_at: String,
+    date: String,
+    hours: Number,
+    name: String,
+    price: Number,
+    sheet_id: oneOf("sheet"),
+    title: String,
+  },
+  report: {
+    id: primaryKey(dbIndexCounter),
+    accident_premium: Number,
+    date: String,
+    disability_pension: Number,
+    pension_contribution: Number,
+    sheet_id: oneOf("sheet"),
+    sickness_contribution: Number,
+    health_contributions: Number,
+    base: Number,
+    expenses: Number,
+    income: Number,
+    pensions_summary: Number,
+    proceeds: Number,
+    social_security: Number,
+    tax: Number,
+  },
+};
+
+export type MockEntity<Key extends keyof typeof directory> = Entity<
+  typeof directory,
+  Key
+>;
+
+type MockValue<Key extends keyof typeof directory> = Value<
+  typeof directory[Key],
+  typeof directory
+>;
+
+export const db = factory(directory);
+
+export const convert = {
+  toSheet: (entity?: MockValue<"sheet"> | null): Sheet | undefined => {
+    if (!entity || !entity.user_id) return undefined;
+    return entity;
+  },
+  toCompany: (entity?: MockValue<"company"> | null): Company | undefined => {
+    if (!entity || !entity.sheet_id?.id) return undefined;
+    return { ...entity, sheet_id: entity.sheet_id?.id };
+  },
+  toInvoice: (entity?: MockValue<"invoice"> | null): Invoice | undefined => {
+    if (!entity || !entity.company_id || !entity.sheet_id) return undefined;
+    const company = convert.toCompany(entity.company_id);
+    if (!company) return undefined;
+    return {
+      ...entity,
+      company_id: entity.company_id?.id,
+      company,
+      sheet_id: entity.sheet_id?.id,
+    };
+  },
+  toReport: (entity?: MockValue<"report"> | null): Report | undefined => {
+    if (!entity || !entity.sheet_id) return undefined;
+    return { ...entity, sheet_id: entity.sheet_id?.id };
+  },
+};
